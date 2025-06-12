857ae8d64073eef6043f7d14e177275d061c547a
SheikhIshere <your@email.com>
2025-06-12 18:59:24 +0600

    done

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Transaction
+from .views import send_transaction_mail
+
+
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
@@ -8,6 +11,14 @@ class TransactionAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.account.balance += obj.amount
         obj.balance_after_transaction = obj.account.balance
-        
         obj.account.save()
+
+        # Send email to the actual account owner, not the admin
+        send_transaction_mail(
+            user=obj.account.user,  # Corrected
+            amount=obj.amount,
+            subject='Loan Approved Message',
+            template='mail/loan_approved_mail.html'
+        )
+        
         super().save_model(request, obj, form, change)
--- transactions/views.py
+++ transactions/views.py
@@ -10,6 +10,19 @@ from datetime import datetime
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from django.urls import reverse_lazy
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+
+def send_transaction_mail(user, amount, subject, template):
+    message = render_to_string(template, {
+        'user': user,
+        'amount': amount,
+    })
+    send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+    send_email.attach_alternative(message, "text/html")
+    send_email.send()
+
 
 class TransactionCreateMixin(LoginRequiredMixin, CreateView):
     template_name = 'transaction_form.html'
@@ -59,6 +72,10 @@ class DepositMoneyView(TransactionCreateMixin):
         account.save(update_fields=['balance'])
         
         messages.success(self.request, f'${amount} was deposited successfully.')
+       
+        # this thing is sending mail to the user's email
+        send_transaction_mail(self.request.user, amount, 'Diposite Messege', 'mail/deposit_mail.html')
+        
         return super().form_valid(form)
 
 class WithdrawMoneyView(TransactionCreateMixin):
@@ -86,6 +103,11 @@ class WithdrawMoneyView(TransactionCreateMixin):
         account.save(update_fields=['balance'])
         
         messages.success(self.request, f'${amount} was withdrawn successfully.')
+
+        # this thing is sending mail to the user's email
+        send_transaction_mail(self.request.user, amount, 'withdrawal Messege', 'mail/withdrawal_mail.html')
+        
+
         return super().form_valid(form)
 
 class LoanRequestView(TransactionCreateMixin):
@@ -104,11 +126,12 @@ class LoanRequestView(TransactionCreateMixin):
             return redirect('home')
         
         current_loan_count = Transaction.objects.filter(
-            account=account, transaction_type=LOAN, loan_approve=True
+            account=account, transaction_type=LOAN, loan_approve=False
         ).count()
         
         if current_loan_count >= 3:
-            return HttpResponse('You have crossed your loan limit of 3 requests.')
+            form.add_error(None, 'You have crossed your loan limit of 3 requests.')
+            return self.form_invalid(form)
         
         # Create loan request (balance not affected until approved)
         transaction = form.save(commit=False)
@@ -117,6 +140,10 @@ class LoanRequestView(TransactionCreateMixin):
         transaction.save()
         
         messages.success(self.request, f'Loan request worth {amount}$ submitted.')
+
+        # this thing is sending mail to the user's email
+        send_transaction_mail(self.request.user, amount, 'Loan Request Messege', 'mail/loan_request_mail.html')
+        
         return super().form_valid(form)
 
 class TransactionReportView(LoginRequiredMixin, ListView):
@@ -150,29 +177,42 @@ class TransactionReportView(LoginRequiredMixin, ListView):
             'balance': self.balance
         })
         return context
+    
+    
 
 class PayLoanView(LoginRequiredMixin, View):
     def get(self, request, loan_id):
         loan = get_object_or_404(Transaction, id=loan_id)
-        if loan.loan_approve:
-            account = getattr(request.user, 'account', None)
-            if not account:
-                messages.error(request, "You don't have an account.")
-                return redirect('home')
-            
-            if loan.amount <= account.balance:
-                account.balance -= loan.amount
-                loan.balance_after_transaction = account.balance
-                account.save()
-                loan.transaction_type = LOAN_PAID
-                loan.save()
-                return redirect('transaction_report')
-            
-            messages.error(request, 'Not enough balance.')
-            return redirect('loan_list')
         
-        messages.error(request, 'Loan not approved yet.')
+        if not loan.loan_approve:
+            messages.error(request, 'Loan not approved yet.')
+            return redirect('loan_list')
+
+        account = getattr(request.user, 'account', None)
+        if not account:
+            messages.error(request, "You don't have an account.")
+            return redirect('home')
+
+        if loan.amount <= account.balance:
+            account.balance -= loan.amount
+            loan.balance_after_transaction = account.balance
+            account.save()
+            loan.transaction_type = LOAN_PAID
+            loan.save()
+
+            # ✅ Send email here
+            send_transaction_mail(
+                user=request.user,
+                amount=loan.amount,
+                subject='Loan Payment Successful',
+                template='mail/loan_payment_mail.html'
+            )
+
+            return redirect('transaction_report')
+
+        messages.error(request, 'Not enough balance.')
         return redirect('loan_list')
+
 
 class LoanListView(LoginRequiredMixin, ListView):  # Fixed typo in class name
     model = Transaction
